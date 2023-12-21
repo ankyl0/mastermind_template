e44df6ab104adc914eb7d044863f6bdf460be70c
ankylo <angelo.dafonte5@gmail.com>
2023-12-21 10:43:48 +0100

    done

--- ai_vs_player.py
+++ ai_vs_player.py
@@ -1,46 +1,93 @@
-from os import system
-from re import search
 from random import randint
-from validator import *
+from itertools import permutations
+from os import system
 
-def generate_guess(previous_guesses, correct_numbers, banned_numbers, target_number):
-    ai_guess : list = []
-    
-    # Generates initial guess of only unique numbers.
-    if len(previous_guesses) == 0:
-        numbers = list(range(1,10))
-        while len(ai_guess) != 4:
-            index = randint(0,10)
+ENTER_PROMPT = '[Press Enter to continue...]'
+
+def generate_code():
+    guess = []
+    numbers = [1,2,3,4,5,6,7,8,9]
+    while len(guess) != 4:
             try:
-                ai_guess.append(numbers[index])
-                numbers.pop(index)
+                guess.append(numbers.pop(randint(0,9)))
             except IndexError:
                 continue
-        return ai_guess
-    
-    else:
-        previous_guess_result = sheet_validator(str(previous_guesses[len(previous_guesses)-1]), target_number)
-        print(previous_guess_result)
-        input()
+    return guess  # Gives code with only unique numbers.
 
+def evaluate_guess(secret_code, guess):
+    exact_matches = sum(s == g for s, g in zip(secret_code, guess))    # Number of exact matches
+    partial_matches = sum(min(secret_code.count(digit), guess.count(digit)) for digit in set(secret_code)) - exact_matches
+
+    return exact_matches, partial_matches
+
+def make_advanced_guess(previous_guesses, previous_feedbacks):
+    if not previous_guesses or not previous_feedbacks:
+        return generate_code()  # If no previous feedback, make a random guess
+
+    all_possible_codes = list(permutations(range(10), 4))
+    filtered_codes = []
+
+    # Filter possible codes based on previous feedback
+    for code in all_possible_codes:
+        valid = True
+        for prev_guess, prev_feedback in zip(previous_guesses, previous_feedbacks):
+            exact_matches, partial_matches = evaluate_guess(list(code), prev_guess)
+            if (exact_matches, partial_matches) != prev_feedback:
+                valid = False
+                break
+        if valid:
+            filtered_codes.append(list(code))
+
+    # Choose the code that maximizes information gain
+    best_guess = None
+    best_info_gain = -1
+
+    # Loops through all filtered codes.
+    for code in filtered_codes:
+        info_gain = calculate_information_gain(code, filtered_codes)
+        if info_gain >= best_info_gain:
+            best_guess = code
+            best_info_gain = info_gain
+
+    if best_guess != None:  # Ensures program doesn't return None (will crash otherwise :()      
+        return best_guess
+    else:
+        return generate_code()
+
+def calculate_information_gain(code, possible_codes):
+    # Calculates how much information given by each code.
+    info_gain = 0
+    for possible_code in possible_codes:
+        _, _ = evaluate_guess(code, possible_code)
+        info_gain += 1
+    return info_gain
+        
 def ai_vs_player():
     # Declaring variables.
-    target_number : str = '3232'
-    ai_guess : str = None
-    result_sheet : list = []
-    previous_guesses : list = []
-    round : int = 1
-
-    # Declaring variables used in "generate_guess" function.
-    banned_numbers = []
-    correct_numbers = []
+    code = [int(digit) for digit in input("Enter the secret code (4 digits): ")]
+    round = 0
+    previous_guesses = []
+    previous_feedbacks = []
+    guess = []
     
-    while ai_guess != target_number and round != 13:
-        result_sheet = []
-        ai_guess = generate_guess(previous_guesses, correct_numbers, banned_numbers, target_number)
+    while guess != code and round != 13:
+        guess = make_advanced_guess(previous_guesses, previous_feedbacks)
 
-        previous_guesses.append(ai_guess)
+        # Prints feedback.
+        print(f"[Attempt {round + 1}: {guess}]")
+        exact_matches, partial_matches = evaluate_guess(code, guess)
+        print(f"[Results: {exact_matches} exact matches, {partial_matches} partial matches]\n")
+
+        # Stores feedback for future reference.
+        previous_guesses.append(guess)
+        previous_feedbacks.append((exact_matches, partial_matches))
         round += 1
 
-
+    if guess == code:
+        print(f'[GG, The code was {guess}! I guessed it in {round} rounds!]')
+        input()
+    else:
+        print('[gg, I couldn\'t guess it under 13 rounds :(]')
+        input()
+    system('cls')
     
--- player_vs_pc.py
+++ player_vs_pc.py
@@ -1,6 +1,28 @@
 from os import system
 from random import randint
-from validator import *
+from re import search
+
+ENTER_PROMPT = '[Press Enter to continue...]'
+
+def sheet_validator(user_input, target_number):
+    result_sheet : list = []
+    for i in range(4):  # Loops through the user input.
+        if user_input[i] == target_number[i]:   # Current index is correct.
+            result_sheet.append('R')  
+        elif user_input[i] in target_number:    # Checks if number exists but in wrong position.
+            if len(result_sheet) != 0:
+                for second_i in range(len(result_sheet)):   # Loops through the current result sheet.
+                    if user_input[second_i] == user_input[i] and result_sheet[second_i] == 'X':     # If a number has been marked as X earlier, it will be marked as F instead.
+                        result_sheet.append('F')
+                        break
+                    elif second_i == len(result_sheet)-1:   # Marks number as correct, but in wrong position.
+                        result_sheet.append('X')
+            else:
+                result_sheet.append('X')
+        else:   # Number is not in the target number.
+            result_sheet.append('F')
+
+    return result_sheet
 
 # FIX PRINT STATEMENTS SO THEYT LOOK NICE PLEASE AND THANK YOU 
 def player_vs_pc():
@@ -10,21 +32,26 @@ def player_vs_pc():
     result_sheet : list = []
     round : int = 1
 
-    print(target_number)
+    print(f'[Gamemode - Player vs AI]')
     # Gameloop.
     while target_number != user_input and round != 13:
         result_sheet = []   # Clears result sheet for new round.
-        print('[Enter your guess: ]', end='')
+        print('\n[Enter your guess]: ', end='')
         user_input = str(input())
 
-        result_sheet = sheet_validator(user_input, target_number)
-        print(result_sheet)
-        round += 1
+        if len(user_input) == 4 and search('[a-zA-Z]', user_input) == None:     # Checks that the entered number is valid.
+            result_sheet = sheet_validator(user_input, target_number)
+            print(f'[Round {round} results: {result_sheet}]')
+            round += 1
+        else:
+            print('[INVALID INPUT]')
+        
     
     # Gameloop broken.
     if round <= 12:
-        print('You won!')
+        print(f'\n[GG! You won in {round} rounds!]')
+        input(ENTER_PROMPT)
     else:
-        print('You lost (exceeded 12 rounds.)')
+        print(f'[GG... you didn\'t guess the number in 12 rounds.]')
+        input(ENTER_PROMPT)
     system('cls')
-                
